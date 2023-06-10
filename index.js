bdaec0ca23a07ed2baae37d4fa580d1612119288
Mohamed Yusuf <study@MohamedYusufs-MacBook-Pro.local>
2023-06-10 18:00:26 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+
+
+let employee = {
+  name: "basic",
+  streetAddress: "unknown",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+  const newObj = { ...employee };
+  newObj[key] = value;
+  return newObj;
+}
+
+function deleteFromEmployeeByKey(employees, key){
+  const newObj = {...employees} 
+  delete newObj[key]
+  return newObj
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+   employee[key] = value;
+   return employee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(e, key)
+{
+  Reflect.deleteProperty(e, key);
+  return e;
+}
+
